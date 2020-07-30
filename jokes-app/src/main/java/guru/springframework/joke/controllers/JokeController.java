ea41c17cfcbd3448813c95265d03e5f6185906a0
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-30 17:27:59 +0800

    jokes-app/: generate and display random Chuck Norris jokes. completed

---
+++ jokes-app/src/main/java/guru/springframework/joke/controllers/JokeController.java
@@ -0,0 +1,26 @@
+package guru.springframework.joke.controllers;
+
+import guru.springframework.joke.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+
+    private final JokeService jokeService;
+
+    @Autowired  //optional
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "chucknorris";
+    }
+}
