f730d12cbb501f4d589e940fbaf069159baf3810
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-07-31 16:21:54 +0800

    di/: external properties - add fake data source

--- di/src/main/java/guru/springframework/di/DiApplication.java
+++ di/src/main/java/guru/springframework/di/DiApplication.java
@@ -1,6 +1,7 @@
 package guru.springframework.di;
 
 import guru.springframework.di.controllers.*;
+import guru.springframework.di.examplebeans.FakeDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -12,10 +13,9 @@ public class DiApplication {
         ApplicationContext ctx = SpringApplication.run(DiApplication.class, args);
 
         MyController controller = (MyController) ctx.getBean("myController");
-        System.out.println(controller.hello());
 
-        System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
-        System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+        FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
+
+        System.out.println(fakeDataSource.getUser());
     }
 }
