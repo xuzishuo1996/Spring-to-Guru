5f7bae9b65732649dc19b13bc8bda000510fb490
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-22 21:21:07 +0800

    sfg-jms/: config for local ActiveMQ in docker

--- sfg-jms/src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
+++ sfg-jms/src/main/java/guru/springframework/sfgjms/SfgJmsApplication.java
@@ -1,8 +1,5 @@
 package guru.springframework.sfgjms;
 
-import org.apache.activemq.artemis.core.config.impl.ConfigurationImpl;
-import org.apache.activemq.artemis.core.server.ActiveMQServer;
-import org.apache.activemq.artemis.core.server.ActiveMQServers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,13 +10,14 @@ public class SfgJmsApplication {
 
         // set up the embedded ActiveMQ server
         // see http://activemq.apache.org/components/artemis/documentation/latest/embedding-activemq.html
-        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
-        .setPersistenceEnabled(false)
-        .setJournalDirectory("target/data/journal")
-        .setSecurityEnabled(false)
-        .addAcceptorConfiguration("invm", "vm://0"));
-
-        server.start();
+        // actually, if the server is in the classpath, Spring Boot will auto-config a server for us
+//        ActiveMQServer server = ActiveMQServers.newActiveMQServer(new ConfigurationImpl()
+//        .setPersistenceEnabled(false)
+//        .setJournalDirectory("target/data/journal")
+//        .setSecurityEnabled(false)
+//        .addAcceptorConfiguration("invm", "vm://0"));
+//
+//        server.start();
 
         SpringApplication.run(SfgJmsApplication.class, args);
     }
